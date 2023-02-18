9148eadc5d33256762e9e08b3241911bf4645a38
Ethan <757819951@qq.com>
2023-02-18 17:45:24 +0800

    用户注册表单

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -12,8 +12,8 @@ class UsersTableSeeder extends Seeder
         User::factory()->count(50)->create();
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'Ethan';
+        $user->email = 'Ethan@example.com';
         $user->save();
     }
 }
